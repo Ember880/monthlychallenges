81b118c684203a0e1ab91c03af68b54528a71df2
bright <brightdeember7000@gmail.com>
2023-05-18 20:27:08 +0000

    learning django from scratch

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,8 +3,9 @@ from . import  views
 
 
 urlpatterns = [
+    path("",views.index),
     path("<int:month>", views.monthbynumber),
-    path("<str:month>", views.monthly)
+    path("<str:month>", views.monthly, name= "monthchallenge")
 ]
  
   
--- challenges/views.py
+++ challenges/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
-
+from django.http import HttpResponse,HttpResponseRedirect, HttpResponseNotFound
+from django.urls import reverse
 # Create your views here.
 
 monthlychallenges = {
     "january":"hello man",
     "february":"yoo whats good",
     "march":"how are you man",
-    "april":"dont do that",
+    "april":"don't do that",
     "may":"tell me",
     "june":"go back",
     "july":"enjoy the moment",
@@ -18,17 +18,36 @@ monthlychallenges = {
     "december":"party timmmmeeee!"    
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthlychallenges.keys())
+    for mon in months:
+        capitalized_month = mon.capitalize()
+        month_path = reverse("monthchallenge", args=[mon])
+        list_items += f"<li><a href =\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<u>{list_items}</u>"
+    return HttpResponse(response_data)
+
 
 def monthbynumber(request, month):
-    return HttpResponse(month)
+    numbermonth = list(monthlychallenges.keys())
+    if month > len(numbermonth):
+        return HttpResponseNotFound("how many months are on your calendar?")
+    redirect_month = numbermonth[month -1 ]
+
+    redirect_path = reverse("monthchallenge", args=[redirect_month])
+
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
-def monthly(request, month):
+def monthly(request, month):  
     try:
         challenge_rule = monthlychallenges[month]
+        respond_data = f"<h1>{challenge_rule}</h1>"
+        return HttpResponse(respond_data) 
     except:
-        return HttpResponseNotFound("this is man is not available for humans")    
+        return HttpResponseNotFound("this month is not available for humans")    
 
 
     
-    return HttpResponse(challenge_rule)
+     
